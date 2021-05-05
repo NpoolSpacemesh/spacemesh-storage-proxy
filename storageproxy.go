e0500b154dd4576b331281c81747c12c1011885d
Zhao KK <kikakkz@hotmail.com>
2021-05-05 13:53:30 +0800

    Add file post handler

--- storageproxy.go
+++ storageproxy.go
@@ -56,6 +56,40 @@ func (p *StorageProxy) Run() error {
 	return nil
 }
 
+func (p *StorageProxy) postPlotFile(file string) error {
+	minPosting := 100
+	postHost := ""
+
+	p.mutex.Lock()
+	for _, host := range p.config.StorageHosts {
+		postCount, ok := p.postingHosts[host]
+		if !ok {
+			p.postingHosts[host] = 0
+			postHost = host
+			break
+		}
+
+		if postCount < minPosting {
+			minPosting = postCount
+			postHost = host
+		}
+	}
+	p.postingHosts[postHost] += 1
+	p.mutex.Unlock()
+
+	log.Infof(log.Fields{}, "try to post file %v -> %v [%v]", file, postHost, minPosting)
+	// TODO: post plot file to host
+
+	p.mutex.Lock()
+	p.postingHosts[postHost] -= 1
+	if p.postingHosts[postHost] == 0 {
+		delete(p.postingHosts, postHost)
+	}
+	p.mutex.Unlock()
+
+	return nil
+}
+
 func (p *StorageProxy) NewPlotRequest(w http.ResponseWriter, req *http.Request) (interface{}, string, int) {
 	b, err := ioutil.ReadAll(req.Body)
 	if err != nil {
@@ -78,7 +112,7 @@ func (p *StorageProxy) NewPlotRequest(w http.ResponseWriter, req *http.Request) 
 			log.Infof(log.Fields{}, "%v | %v is not regular plot file", input.PlotDir, path)
 			return nil
 		}
-		return nil
+		return p.postPlotFile(path)
 	})
 	if err != nil {
 		return nil, err.Error(), -4
